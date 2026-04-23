6d8c9dd0848d7f6c6d1569a16297adff3a7fd524
Mokshith Sai Jinkala <mokshithsaijinkala@gmail.com>
2026-04-23 10:43:50 +0530

    Upgrade AI demo runtime setup

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import SecureSyscall from "../secure_syscall (1).jsx";
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <SecureSyscall />
+  </React.StrictMode>
+);
